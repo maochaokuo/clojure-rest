86b111960fe16a12bbabd3858f982002145b2300
Thomas Mao <tm1813@gmail.com>
2021-08-29 17:56:31 +0800

    handler.clj type all

--- src/clojure_rest/handler.clj
+++ src/clojure_rest/handler.clj
@@ -25,3 +25,48 @@
       (middleware/wrap-json-body)
       (middleware/wrap-json-response))
   )
+
+(def db-config
+  {:classname "org.h2.Driver"
+   :subprotocol "h2"
+   :subname "mem:documents"
+   :user ""
+   :password ""})
+
+(defn pool
+  [config]
+  (let [cpds (doto (ComboPooledDataSource.)
+               (.setDriverClass (:classname config))
+               (.setJdbcUrl (str "jdbc:" (:subprotocol config) ":" (:subname config)))
+               (.setUser (:user config))
+               (.setPassword (:password config))
+               (.setMaxPoolSize 6)
+               (.setMinPoolSize 1)
+               (.setInitialPoolSize 1))]
+    {:datasource cpds}))
+
+(def pooled-db (delay (pool db-config)))
+
+(defn db-connection [] @pooled-db)
+
+(sql/with-connection (db-connection)
+                     (sql/create-table :documents [:id "varchar(256)" "primary key"]
+                                       [:title "varchar(1024)"]
+                                       [:text :varchar]))
+
+(defn get-document [id]
+  (sql/with-connection (db-connection)
+                       (sql/with-query-results results
+                                               ["select * from documents where id = ?" id]
+                                               (cond
+                                                 (empty? results) {:status 404}
+                                                 :else (response (first results))))))
+
+(defn uuid [] (str (java.util.UUID/randomUUID)))
+
+(defn create-new-document [doc]
+  (let [id (uuid)]
+    (sql/with-connection (db-connection)
+                         (let [document (assoc doc "id" id)]
+                           (sql/insert-record :documents document)))
+    (get-document id)))
