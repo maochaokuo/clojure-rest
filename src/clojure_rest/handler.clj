33a41bbca73b42fe11bf7b5636821c52c09860bb
Thomas Mao <tm1813@gmail.com>
2021-08-29 17:53:40 +0800

    handler.clj update defroutes

--- src/clojure_rest/handler.clj
+++ src/clojure_rest/handler.clj
@@ -1,11 +1,25 @@
 (ns clojure-rest.handler
   (:use compojure.core)
-  (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+  (:require ;[compojure.core :refer :all]
+    [compojure.handler :as handler]
+    [compojure.route :as route]
+    ;[ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    ))
 
 (defroutes app-routes
-           (GET "/" [] "Hello World")
-           (route/not-found "Not Found"))
+  (context "/documents" [] (defroutes documents-routes
+   (GET  "/" [] (get-all-documents))
+   (POST "/" {body :body} (create-new-document body))
+   (context "/:id" [id] (defroutes document-routes
+     (GET    "/" [] (get-document id))
+     (PUT    "/" {body :body} (update-document id body))
+     (DELETE "/" [] (delete-document id))))))
+  (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  ;(wrap-defaults app-routes site-defaults)
+  ;(handler/site app-routes)
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response))
+  )
