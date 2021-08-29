5c4eaf56bfd70b9cd1e20184a580c23298a5eb7d
Thomas Mao <tm1813@gmail.com>
2021-08-29 17:54:21 +0800

    handler.clj update ns

--- src/clojure_rest/handler.clj
+++ src/clojure_rest/handler.clj
@@ -1,10 +1,12 @@
 (ns clojure-rest.handler
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource)
   (:use compojure.core)
-  (:require ;[compojure.core :refer :all]
-    [compojure.handler :as handler]
-    [compojure.route :as route]
-    ;[ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-    ))
+  (:use cheshire.core)
+  (:use ring.util.response)
+  (:require [compojure.handler :as handler]
+            [ring.middleware.json :as middleware]
+            [clojure.java.jdbc :as sql]
+            [compojure.route :as route]))
 
 (defroutes app-routes
   (context "/documents" [] (defroutes documents-routes
